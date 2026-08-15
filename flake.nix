b17b3fa97d931d6a931c6820f05723bac2891fde
Miccah <dev@miccah.io>
2026-08-15 13:46:42 -0700

    Add template flake files

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "xdp";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        # Add packages here.
+      ];
+    };
+  };
+}
